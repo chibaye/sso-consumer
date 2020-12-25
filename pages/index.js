2164dca46617b8bb04cfe06d3b417318c8e7cce0
Francis Chibaye <francischibaye@gmail.com>
2020-12-25 23:09:28 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import {parse} from 'cookie'
 
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+
 export const getServerSideProps = async ctx => {
     const {req, res} = ctx
     const {session} = parse(req.headers?.cookie || '')
@@ -8,6 +13,13 @@ export const getServerSideProps = async ctx => {
     if (!session) {
         res.writeHead(303, {Location: 'https://sso-server.vercel.app/login?refid=consumer'})
         res.end()
+    }
+
+    if (req.uri.match(/refid/gi)) {
+        await fetch('/api/session', {
+            headers, method: 'POST',
+            body: JSON.stringify({ssoToken: req.url.split('=')[1]})
+        })
     }
 
     return {
