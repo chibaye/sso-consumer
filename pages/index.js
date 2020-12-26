efff472f5cce7b2f3a50dba60858358eea90af56
Francis Chibaye <francischibaye@gmail.com>
2020-12-26 09:17:36 +0200

    init

--- pages/index.js
+++ pages/index.js
@@ -38,8 +38,8 @@ export const getServerSideProps = async ctx => {
     }
 }
 
-const Home = props => <div>
-    {JSON.stringify(props)}
-</div>
+const Home = () => <h1>
+    You're viewing a secure page for the SSO Consumer app
+</h1>
 
 export default Home
